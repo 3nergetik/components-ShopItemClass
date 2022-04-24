de62073f68698e86fbefce5c0e46fddf48a23749
3nergetik <carbo_r86@mail.ru>
2022-04-24 21:56:36 +0500

    ready-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './css/main.css';
+import PropTypes from 'prop-types';
+
+import { ShopItemClass } from './ShopItemClass';
+import ItemCard from './ItemCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const item = new ShopItemClass({
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: 'GBP'
+  });
+
+  return ItemCard(item);
+}
+
+ShopItemClass.propTypes = {
+  item: PropTypes.instanceOf(ShopItemClass).isRequired
 }
 
 export default App;
